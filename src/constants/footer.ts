d720079d5c92630b8bb36b658f13ee13873a6f6e
devsenpai09@gmail.com <devsenpai09@gmail.com>
2024-03-19 18:10:53 +0100

    update socials

--- src/constants/footer.ts
+++ src/constants/footer.ts
@@ -5,13 +5,21 @@ export const FOOTER_LINKS = {
     { title: "Messenger", url: "" },
   ],
   socials: [
-    { alt: "Youtube Icon", icon_url: "/assets/youtube.svg", url: "" },
+    {
+      alt: "Discord Icon",
+      icon_url: "/assets/blue-white-discord.svg",
+      url: "https://discord.gg/M73gWMTU",
+    },
+    {
+      alt: "Telegram Icon",
+      icon_url: "/assets/telegram.svg",
+      url: "https://t.me/trufunderpropfirm",
+    },
     {
       alt: "Instagram Icon",
       icon_url: "/assets/instagram.svg",
       url: "https://www.instagram.com/tru.funder?igsh=bnltYTdhdDB2ODY3&utm_source=qr",
     },
-    { alt: "Tiktok Icon", icon_url: "/assets/tiktok.svg", url: "" },
     {
       alt: "Twitter Icon",
       icon_url: "/assets/twitter.svg",
