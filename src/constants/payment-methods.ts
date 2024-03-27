e6725b070c129b5b551660c8fad64f020d94afe1
devsenpai09@gmail.com <devsenpai09@gmail.com>
2024-03-27 16:18:45 +0100

    update trc wallet

--- src/constants/payment-methods.ts
+++ src/constants/payment-methods.ts
@@ -2,7 +2,7 @@ export const PAYMENT_METHODS = {
   btc: { coin: "BTC", address: "1kPLHZutsrkFsf9DoTPuhDkQs6uKaTvb1" },
   "usdt-trc-20": {
     coin: "USDT (Trc 20)",
-    address: "0xcd3327eacc9ed54f7c17f6d0b932d5cdb429f1d2",
+    address: "TM9Nhjsonrw5f15ePBoJG6cXpw5xZ5aggb",
   },
   "usdt-erc-20": {
     coin: "USDT (Erc 20)",
