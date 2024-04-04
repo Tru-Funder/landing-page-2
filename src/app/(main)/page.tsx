889b30f1404dd1db12094858e8d4c80f8f50cf93
devsenpai09@gmail.com <devsenpai09@gmail.com>
2024-04-04 23:03:35 +0100

    update

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -15,7 +15,6 @@ export default function Page() {
       <TrufunderChallengeTypes />
       <CompareOurChallenges />
       {/* <TradeOurProducts /> */}
-      <JoinCommunity />
     </main>
   );
 }
